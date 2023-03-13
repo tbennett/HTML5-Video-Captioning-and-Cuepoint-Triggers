5d8b6748c536c876853740db9c57f7609bd16e76
Troy <tbennett@dvc.edu>
2023-03-12 20:55:42 -0700

    Added starter content to iframe

--- j/modules/cuepoints.js
+++ j/modules/cuepoints.js
@@ -37,13 +37,12 @@ const CueTimer = () => {
 	let previous_cue;	// flag to prevent callback being triggered more than once.
 
 	const init = function(vidId, cuesArray) {
-
 		//add the event listener to the video element
 		document.getElementById(vidId).addEventListener('timeupdate', (e) => 
 		{
 			const video = e.target || window.srcElement;
 			let now = Math.floor(video.currentTime);
-
+		
 			// loop through all cuepoints looking for a current cue
 			for (let i in cuesArray) 
 			{
